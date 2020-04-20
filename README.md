401723b9bf0fff3e6ee0576046e2865b81a15078
Marc Zych <marczych@gmail.com>
2020-04-19 21:26:49 -0700

    Add commands argument and service file

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Repository of instructions, links, and webserver code for building an API for
 sending IR commands on a Raspberry Pi.
+
+## Recording Commands
+
+```bash
+ir-ctl --device /dev/lirc1 --receive > command_name
+```
--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
+import os
 import subprocess
 
 import tornado.ioloop
 import tornado.web
 
 
+# Populated during startup.
+CONFIG = dict(
+    command_directory=None,
+    commands=None,
+)
+
+
 class SendHandler(tornado.web.RequestHandler):
     def post(self):
         command = json.loads(self.request.body)["command"]
 
-        if command not in ("volup", "voldown"):
+        if command not in CONFIG["commands"]:
             self.set_status(400)
             self.write(dict(error=f"Unknown command: {command}"))
             return
@@ -22,7 +31,7 @@ class SendHandler(tornado.web.RequestHandler):
                 "--device",
                 "/dev/lirc0",
                 "--send",
-                command,
+                os.path.join(CONFIG["command_directory"], command),
             ],
         )
 
@@ -35,7 +44,20 @@ def make_app():
     ])
 
 
+def configure(command_directory: str) -> None:
+    CONFIG["command_directory"] = command_directory
+    CONFIG["commands"] = set(os.listdir(command_directory))
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command_directory")
+    args = parser.parse_args()
+
+    configure(args.command_directory)
+
+    print(f"Running with config: {CONFIG}")
+
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
